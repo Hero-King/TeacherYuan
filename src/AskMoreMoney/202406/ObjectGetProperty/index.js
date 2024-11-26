8e5c1beb349f103e370589c9577ad100bc75b508
HeroKing <heroking@HeroKingdeMac-mini.local>
2024-11-26 22:56:39 +0800

    feat: 访问对象属性的两种方式区别

---
+++ src/AskMoreMoney/202406/ObjectGetProperty/index.js
@@ -0,0 +1,15 @@
+const obj = {};
+
+// 两种方式的区别到底是什么??
+obj.name = 'João'; // [[GET]](obj, 'name', obj); 最后一个参数为this指向
+obj['name'] = 'Maria'; // [[GET]])(obj, name是Symbol ? name : String(name), obj)
+
+let test = {
+  toString() {
+    return 'aaa';
+  },
+};
+
+test[test] = 'xxx';
+
+console.log(test);
